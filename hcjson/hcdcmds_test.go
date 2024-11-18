62dfe2eb27db5f98a9d31359e6cc90f2bac6035d
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 05:23:24 +0700

    Update hcdcmds_test.go

--- hcjson/hcdcmds_test.go
+++ hcjson/hcdcmds_test.go
@@ -12,7 +12,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/james-ray/hcd/hcjson"
+	"github.com/iromath/hcd/hcjson"
 )
 
 // TestBtcdCmds tests all of the btcd extended commands marshal and unmarshal
