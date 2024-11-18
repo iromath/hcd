18c9aba4a6139dce1e0312698fb8178110ca160c
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 03:35:45 +0700

    Update mining.go

--- mining.go
+++ mining.go
@@ -14,15 +14,15 @@ import (
 	"sort"
 	"time"
 
-	"github.com/james-ray/hcd/blockchain"
-	"github.com/james-ray/hcd/blockchain/stake"
-	"github.com/james-ray/hcd/chaincfg"
-	"github.com/james-ray/hcd/chaincfg/chainhash"
-	"github.com/james-ray/hcd/hcutil"
-	"github.com/james-ray/hcd/mempool"
-	"github.com/james-ray/hcd/mining"
-	"github.com/james-ray/hcd/txscript"
-	"github.com/james-ray/hcd/wire"
+	"github.com/iromath/hcd/blockchain"
+	"github.com/iromath/hcd/blockchain/stake"
+	"github.com/iromath/hcd/chaincfg"
+	"github.com/iromath/hcd/chaincfg/chainhash"
+	"github.com/iromath/hcd/hcutil"
+	"github.com/iromath/hcd/mempool"
+	"github.com/iromath/hcd/mining"
+	"github.com/iromath/hcd/txscript"
+	"github.com/iromath/hcd/wire"
 )
 
 const (
