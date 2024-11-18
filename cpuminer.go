1af9abef0a4544cad9901cd6fb92570e78d36e42
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 03:28:06 +0700

    Update cpuminer.go

--- cpuminer.go
+++ cpuminer.go
@@ -13,12 +13,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/james-ray/hcd/blockchain"
-	"github.com/james-ray/hcd/chaincfg"
-	"github.com/james-ray/hcd/chaincfg/chainhash"
-	"github.com/james-ray/hcd/hcutil"
-	"github.com/james-ray/hcd/mining"
-	"github.com/james-ray/hcd/wire"
+	"github.com/iromath/hcd/blockchain"
+	"github.com/iromath/hcd/chaincfg"
+	"github.com/iromath/hcd/chaincfg/chainhash"
+	"github.com/iromath/hcd/hcutil"
+	"github.com/iromath/hcd/mining"
+	"github.com/iromath/hcd/wire"
 )
 
 const (
