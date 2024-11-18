3d5eed4a17dda943c9c35e2f673faeace308d438
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 03:25:44 +0700

    Update chain.go

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -14,13 +14,13 @@ import (
 	"sync"
 	"time"
 
-	"github.com/james-ray/hcd/blockchain/stake"
-	"github.com/james-ray/hcd/chaincfg"
-	"github.com/james-ray/hcd/chaincfg/chainhash"
-	"github.com/james-ray/hcd/database"
-	"github.com/james-ray/hcd/hcutil"
-	"github.com/james-ray/hcd/txscript"
-	"github.com/james-ray/hcd/wire"
+	"github.com/iromath/hcd/blockchain/stake"
+	"github.com/iromath/hcd/chaincfg"
+	"github.com/iromath/hcd/chaincfg/chainhash"
+	"github.com/iromath/hcd/database"
+	"github.com/iromath/hcd/hcutil"
+	"github.com/iromath/hcd/txscript"
+	"github.com/iromath/hcd/wire"
 )
 
 const (
