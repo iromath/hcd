33aedd2e7c04708e6467519da9a38862cbb52aa9
iromath <52466736+iromath@users.noreply.github.com>
2024-11-19 22:16:13 +0700

    Update message.go

--- wire/message.go
+++ wire/message.go
@@ -12,7 +12,7 @@ import (
 	"io"
 	"unicode/utf8"
 
-	"github.com/james-ray/hcd/chaincfg/chainhash"
+	"github.com/iromath/hcd/chaincfg/chainhash"
 )
 
 // MessageHeaderSize is the number of bytes in a HC message header.
